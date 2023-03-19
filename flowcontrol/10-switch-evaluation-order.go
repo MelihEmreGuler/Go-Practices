a978f0f412922f09953e6018996ddc8491517d6b
MelihEmreGuler <melihemreguler@hotmail.com>
2023-03-19 16:26:03 +0300

    different type of switch

--- flowcontrol/10-switch-evaluation-order.go
+++ flowcontrol/10-switch-evaluation-order.go
@@ -21,4 +21,32 @@ func main() {
 	default:
 		fmt.Println("Too far away.")
 	}
+
+	a := 2
+
+	switch a { //fallthrough is used to execute the next case
+	case 1:
+		fmt.Println("a is 1")
+		fallthrough
+	case 2:
+		fmt.Println("a is 2")
+		fallthrough
+	case 3:
+		fmt.Println("a is 3")
+		fallthrough
+	case 4:
+		fmt.Println("a is 4")
+		fallthrough
+	default:
+		fmt.Println("a is not 1,2,3,4")
+	}
+
+	switch a := 10; { //switch with no condition is same as switch true
+	case a > 0:
+		fmt.Println("a is greater than 0")
+	case a < 0:
+		fmt.Println("a is less than 0")
+	default:
+		fmt.Println("a is 0")
+	}
 }
