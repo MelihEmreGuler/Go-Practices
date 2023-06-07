95e2a82ceb0846e4f0bd5eba619cad5df73a1c34
Melih Emre Güler <melihguler@Melihs-MacBook-Pro.local>
2023-06-07 23:27:45 +0300

    add map exercises

--- map/main.go
+++ map/main.go
@@ -2,39 +2,51 @@ package main
 
 import "fmt"
 
+type person struct {
+	fname string
+	lname string
+}
+
 func main() {
-	/*
-		colors := map[string]string{
-			"red":   "#ff0000",
-			"green": "#00ff00",
-		}
-	*/
 
-	//var colors map[string]string
-	/*
-		colors := make(map[string]string)
+	colors1 := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+	}
 
-		colors["white"] = "#ffffff" //map' imizce yeni bir deger ekledik
+	//var colors map[string]string //map' in degeri nil'dir, nil map' e deger ekleyemeyiz
 
-		fmt.Println(colors)
+	colors2 := make(map[string]string)
+	fmt.Println("colors2:", colors2)
 
-		delete(colors, "white") //map' imizde key ini girdigimiz degeri sildik
+	colors1["white"] = "#ffffff" //map' imizce yeni bir deger ekledik
+	fmt.Println("colors1:", colors1)
 
-		fmt.Println(colors)
+	delete(colors1, "white") //map' imizde key ini girdigimiz degeri sildik
 
-	*/
+	fmt.Println("colors1", colors1)
 
-	colors := map[string]string{
+	colors3 := map[string]string{
 		"red":   "#ff0000",
 		"green": "#00ff00",
 		"blue":  "#0000ff",
 		"white": "#ffffff",
 	}
-	printMap(colors)
+	printMap(colors3)
 
+	myMap := make(map[string]person)
+
+	myMap["me"] = person{fname: "Melih Emre", lname: "Guler"}
+
+	myMap["me"].printPerson()
 }
 func printMap(c map[string]string) {
 	for color, hex := range c {
 		fmt.Println("Hex code for", color, "is", hex)
 	}
+}
+
+func (p person) printPerson() person {
+	fmt.Printf("first name: %s, last name: %s\n", p.fname, p.lname)
+	return p
 }
